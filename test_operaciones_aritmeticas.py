236d215b35e59d9d8231a843702ffdfdeded0796
LAB_J205 <labj205@continental.edu.pe>
2025-04-11 13:16:51 -0500

    Se implemento la division

---
+++ test_operaciones_aritmeticas.py
@@ -0,0 +1,48 @@
+import unittest
+from operaciones_aritmeticas import OperacionesAritmeticas
+
+class TestOperacionesAritmeticas(unittest.TestCase):
+    def test_suma_dosNumeros_retornaSuma(self):
+        # Arrange
+        operando1 = 10
+        operando2 = 15
+        resultadoEsperado = 25
+
+        obj = OperacionesAritmeticas(operando1, operando2)
+
+        # Act
+        resultadoActual = obj.calcularSuma()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual, "Falló la suma")
+
+    def test_suma_operadorNoNumerico_lanzaExcepcion(self):
+        with self.assertRaises(TypeError):
+            obj = OperacionesAritmeticas(3, "a")
+            obj.calcularSuma()
+
+    def test_division_dosNumeros_retornaDivision(self):
+        # Arrange
+        operando1 = 10
+        operando2 = 15
+        resultadoEsperado = 0.666
+
+        obj = OperacionesAritmeticas(operando1, operando2)
+
+        # Act
+        resultadoActual = obj.calcular_division()
+
+        # Assert
+        self.assertAlmostEqual(resultadoEsperado, resultadoActual, places=2, msg="Falló la división")
+
+    def test_division_operadorNoNumerico_lanzaExcepcion(self):
+        with self.assertRaises(TypeError):
+            obj = OperacionesAritmeticas(3, "a")
+            obj.calcular_division()
+
+    def test_division_divisorCero_lanzaExcepcion(self):
+        with self.assertRaises(ZeroDivisionError):
+            obj = OperacionesAritmeticas(3, "0")
+            obj.calcular_division()
+if __name__ == '__main__':
+    unittest.main()
